4afac55c12fb899cd5a9604f37b6e54a6427dfa9
Kunal Hole <kunalhole538@gmail.com>
2022-09-23 16:41:22 +0530

    UI: No Metamask

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,34 +1,39 @@
-import Link from 'next/link'
+import Link from "next/link"
 
 type Props = {
-	btnName: string
-	walletAddress: string | null
+  btnName: string
+  walletAddress: string | null
 }
 
 const Header = ({ btnName, walletAddress }: Props) => {
-	return (
-		<header className='flex justify-between px-8 pt-8 w-full'>
-			<h1 className='text-[32px] font-bold text-secondary'>MultiSend</h1>
-			{walletAddress ? (
-				<div className='space-x-8'>
-					<h4 className='inline text-rare/75 font-medium'>
-						{walletAddress.substring(0, 5)}...{walletAddress.substring(38)}
-					</h4>
-					<Link href={`${btnName === 'Go Home' ? '/' : '/dashboard'}`}>
-						<button className='bg-secondary font-medium text-base rounded-md px-4 py-[12px] text-primary hover:bg-rare hover:scale-105 transition'>
-							{btnName}
-						</button>
-					</Link>
-				</div>
-			) : (
-				<Link href={`${btnName === 'Go Home' ? '/' : '/dashboard'}`}>
-					<button className='bg-secondary font-medium text-base rounded-md px-4 py-[12px] text-primary hover:bg-rare hover:scale-105 transition'>
-						{btnName}
-					</button>
-				</Link>
-			)}
-		</header>
-	)
+  return (
+    <header className="flex justify-between px-8 pt-8 w-full">
+      <Link href={"/"}>
+        <h1 className="text-[32px] cursor-pointer font-bold text-secondary">
+          MultiSend
+        </h1>
+      </Link>
+
+      {walletAddress ? (
+        <div className="space-x-8">
+          <h4 className="inline text-rare/75 font-medium">
+            {walletAddress.substring(0, 5)}...{walletAddress.substring(38)}
+          </h4>
+          <Link href={`${btnName === "Go Home" ? "/" : "/dashboard"}`}>
+            <button className="bg-secondary font-medium text-base rounded-md px-4 py-[12px] text-primary hover:bg-rare hover:scale-105 transition">
+              {btnName}
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <Link href={`${btnName === "Go Home" ? "/" : "/dashboard"}`}>
+          <button className="bg-secondary font-medium text-base rounded-md px-4 py-[12px] text-primary hover:bg-rare hover:scale-105 transition">
+            {btnName}
+          </button>
+        </Link>
+      )}
+    </header>
+  )
 }
 
 export default Header
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,114 +1,115 @@
-import Guest from '@/components/Guest'
-import SwitchNetwork from '@/components/SwitchNetwork'
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootDispatch, RootState } from '@/store/index'
+import Guest from "@/components/Guest"
+import SwitchNetwork from "@/components/SwitchNetwork"
+import { useEffect, useState } from "react"
+import Head from "next/head"
+import { useDispatch, useSelector } from "react-redux"
+import { RootDispatch, RootState } from "@/store/index"
 import {
-	setDashboard,
-	setDifferentChain,
-	setNoMetamask,
-	setUnAuth,
-} from '@/store/dashboardState'
-import detectEthereumProvider from '@metamask/detect-provider'
-import Dashboard from '@/components/Dashboard'
+  setDashboard,
+  setDifferentChain,
+  setNoMetamask,
+  setUnAuth,
+} from "@/store/dashboardState"
+import detectEthereumProvider from "@metamask/detect-provider"
+import Dashboard from "@/components/Dashboard"
+import NoMetamask from "@/components/NoMetamask"
 
 const dashboard = () => {
-	const [metamask, setMetamask] = useState<null | any>(null)
-	const { status } = useSelector<
-		RootState,
-		{ status: 'unAuth' | 'differentChain' | 'dashboard' | 'noMetamask' }
-	>((state) => ({
-		status: state.dashboard.status,
-	}))
-	const dispatch: RootDispatch = useDispatch()
+  const [metamask, setMetamask] = useState<null | any>(null)
+  const { status } = useSelector<
+    RootState,
+    { status: "unAuth" | "differentChain" | "dashboard" | "noMetamask" }
+  >((state) => ({
+    status: state.dashboard.status,
+  }))
+  const dispatch: RootDispatch = useDispatch()
 
-	// Check If There is Provider
-	useEffect(() => {
-		async function detectProvider() {
-			const provider = (await detectEthereumProvider()) as any
-			return provider
-		}
+  // Check If There is Provider
+  useEffect(() => {
+    async function detectProvider() {
+      const provider = (await detectEthereumProvider()) as any
+      return provider
+    }
 
-		detectProvider()
-			.then((provider) => {
-				if (provider) setMetamask(provider)
-			})
-			.catch((error) => {
-				console.log(error)
-				dispatch(setNoMetamask())
-			})
-	}, [])
+    detectProvider()
+      .then((provider) => {
+        if (provider) setMetamask(provider)
+      })
+      .catch((error) => {
+        console.log(error)
+        dispatch(setNoMetamask())
+      })
+  }, [])
 
-	useEffect(() => {
-		const accountsChangedHandler = (accounts: string[]) => {
-			if (!accounts.length) {
-				dispatch(setUnAuth())
-			} else {
-				if (metamask.chainId === '0x5') {
-					dispatch(setDashboard())
-				} else {
-					dispatch(setDifferentChain())
-				}
-			}
-		}
+  useEffect(() => {
+    const accountsChangedHandler = (accounts: string[]) => {
+      if (!accounts.length) {
+        dispatch(setUnAuth())
+      } else {
+        if (metamask.chainId === "0x5") {
+          dispatch(setDashboard())
+        } else {
+          dispatch(setDifferentChain())
+        }
+      }
+    }
 
-		const chainChangedHandler = (chainId: string) => {
-			if (metamask.selectedAddress) {
-				if (chainId !== '0x5') dispatch(setDifferentChain())
-				else dispatch(setDashboard())
-			} else {
-				dispatch(setUnAuth())
-			}
-		}
+    const chainChangedHandler = (chainId: string) => {
+      if (metamask.selectedAddress) {
+        if (chainId !== "0x5") dispatch(setDifferentChain())
+        else dispatch(setDashboard())
+      } else {
+        dispatch(setUnAuth())
+      }
+    }
 
-		if (metamask) {
-			metamask.on('accountsChanged', accountsChangedHandler)
-			metamask.on('chainChanged', chainChangedHandler)
-		} else dispatch(setNoMetamask())
+    if (metamask) {
+      metamask.on("accountsChanged", accountsChangedHandler)
+      metamask.on("chainChanged", chainChangedHandler)
+    } else dispatch(setNoMetamask())
 
-		return () => {
-			metamask?.removeListener('accountsChanged', accountsChangedHandler)
-			metamask?.removeListener('chainChanged', chainChangedHandler)
-		}
-	}, [metamask])
+    return () => {
+      metamask?.removeListener("accountsChanged", accountsChangedHandler)
+      metamask?.removeListener("chainChanged", chainChangedHandler)
+    }
+  }, [metamask])
 
-	// User State
-	useEffect(() => {
-		if (metamask) {
-			if (metamask.selectedAddress) {
-				if (metamask.chainId !== '0x5') dispatch(setDifferentChain())
-				else {
-					dispatch(setDashboard())
-				}
-			} else dispatch(setUnAuth())
-		} else dispatch(setNoMetamask())
-	}, [metamask])
+  // User State
+  useEffect(() => {
+    if (metamask) {
+      if (metamask.selectedAddress) {
+        if (metamask.chainId !== "0x5") dispatch(setDifferentChain())
+        else {
+          dispatch(setDashboard())
+        }
+      } else dispatch(setUnAuth())
+    } else dispatch(setNoMetamask())
+  }, [metamask])
 
-	return (
-		<>
-			<Head>
-				<title>
-					{status === 'unAuth'
-						? 'Connect to MultiSend'
-						: status === 'differentChain'
-						? 'Switch Network'
-						: 'Dashboard'}
-				</title>
-				<link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
-			</Head>
-			{status === 'unAuth' ? (
-				<Guest metamask={metamask} />
-			) : status === 'differentChain' ? (
-				<SwitchNetwork metamask={metamask} />
-			) : status === 'dashboard' ? (
-				<Dashboard currentUser={metamask?.selectedAddress} />
-			) : (
-				// <div>You are on dashboard {metamask?.selectedAddress}</div>
-				<div>No metamask installed</div>
-			)}
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>
+          {status === "unAuth"
+            ? "Connect to MultiSend"
+            : status === "differentChain"
+            ? "Switch Network"
+            : "Dashboard"}
+        </title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      {status === "unAuth" ? (
+        <Guest metamask={metamask} />
+      ) : status === "differentChain" ? (
+        <SwitchNetwork metamask={metamask} />
+      ) : status === "dashboard" ? (
+        <Dashboard currentUser={metamask?.selectedAddress} />
+      ) : (
+        // <div>You are on dashboard {metamask?.selectedAddress}</div>
+        <NoMetamask />
+      )}
+    </>
+  )
 }
 
 export default dashboard
