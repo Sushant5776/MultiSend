bceb98910a12d87a0282a26d2a044b92030dc415
Kunal Hole <kunalhole538@gmail.com>
2022-09-23 16:18:07 +0530

    feature implementation: loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,13 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { Provider as StateProvider } from "react-redux"
 import { store } from "@/store/index"
+import nprogress from "nprogress"
+import Router from "next/router"
+
+nprogress.configure({ showSpinner: false })
+Router.events.on("routeChangeStart", () => nprogress.start())
+Router.events.on("routeChangeComplete", () => nprogress.done())
+Router.events.on("routeChangeError", () => nprogress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,44 +1,52 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
-	return (
-		<Html>
-			<Head>
-				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<link
-					rel='preconnect'
-					href='https://fonts.gstatic.com'
-					crossOrigin='true'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
-			<body
-				style={{
-					backgroundImage: 'url(/bgCircles.png)',
-					backgroundPosition: 'center',
-					backgroundSize: 'cover',
-					backgroundRepeat: 'repeat-y',
-					backgroundAttachment: 'fixed',
-				}}
-				className='font-poppins w-full min-h-screen relative overflow-hidden'>
-				<div
-					data-id='glow-top-left'
-					className='glow w-80 h-80 -top-40 -left-11'
-				/>
-				<div
-					data-id='glow-top-right'
-					className='glow w-80 h-80 -top-[104px] -right-14'
-				/>
-				<div
-					data-id='bottom-right'
-					className='glow w-[448px] h-[448px] -right-[172px] -bottom-[174px]'
-				/>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	)
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <body
+        style={{
+          backgroundImage: "url(/bgCircles.png)",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "repeat-y",
+          backgroundAttachment: "fixed",
+        }}
+        className="font-poppins w-full min-h-screen relative overflow-hidden"
+      >
+        <div
+          data-id="glow-top-left"
+          className="glow w-80 h-80 -top-40 -left-11"
+        />
+        <div
+          data-id="glow-top-right"
+          className="glow w-80 h-80 -top-[104px] -right-14"
+        />
+        <div
+          data-id="bottom-right"
+          className="glow w-[448px] h-[448px] -right-[172px] -bottom-[174px]"
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
