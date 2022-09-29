afa9759a51e979e382b54ee7fbae580cb8d203ef
Kunal Hole <kunalhole538@gmail.com>
2022-09-29 14:41:49 +0530

    sinking fork

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,16 +1,33 @@
-import Header from '@/components/Header'
+import Header from "@/components/Header"
+import { useEffect } from "react"
+import { utils, Contract, providers, Signer } from "ethers"
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "utils/contractConfig"
 
 interface DashboardProps {
-	currentUser: string | null
+  currentUser: string | null
+  metamask: any
 }
 
-const Dashboard = ({ currentUser }: DashboardProps) => {
-	return (
-		<main>
-			<Header btnName='Go Home' walletAddress={currentUser} />
-			{/* <h1>{currentUser ?? ''}</h1> */}
-		</main>
-	)
+const Dashboard = ({ currentUser, metamask }: DashboardProps) => {
+  // useEffect(() => {
+  //   async function getContract() {
+  //     const provider = new providers.Web3Provider(metamask, "any")
+  //     const signer = provider.getSigner()
+  //     const balance = await signer.getBalance()
+  //     console.log(utils.formatEther(balance))
+
+  //     const myContract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer)
+  //   }
+
+  //   if (metamask) getContract()
+  // }, [metamask])
+
+  return (
+    <main>
+      <Header btnName="Go Home" walletAddress={currentUser} />
+      {/* <h1>{currentUser ?? ''}</h1> */}
+    </main>
+  )
 }
 
 export default Dashboard
