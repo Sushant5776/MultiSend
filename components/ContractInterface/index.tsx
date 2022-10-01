271c676aff4b27b2237cae3d7b96efd8036a0bbc
Sushant Garudkar <garudkar.sa@gmail.com>
2022-10-01 14:11:00 +0530

    MultiSend Feature: Send Payment

--- components/ContractInterface/index.tsx
+++ components/ContractInterface/index.tsx
@@ -1,37 +1,88 @@
-import { useState } from "react"
+import { Contract, utils } from 'ethers'
+import { ChangeEvent, KeyboardEvent, MouseEvent, useState } from 'react'
 
 interface IContractInterfaceProps {
-  balance: string
+	balance: string
+	contract: Contract | undefined
 }
 
-const ContractInterface = ({ balance }: IContractInterfaceProps) => {
-  const [input, setInput] = useState("")
-  //   const [addresses, setAddresses] = useState([])
+const ContractInterface = ({ balance, contract }: IContractInterfaceProps) => {
+	const [addressesString, setAddressesString] = useState('')
+	const [amountToSend, setAmountToSend] = useState('1')
 
-  return (
-    <>
-      <p className="m-4 text-rare/75 text-center font-medium text-sm">
-        MultiSend Balance: <span className="text-secondary">{balance}</span> ETH
-      </p>
-      {/* center form, remove outline, border textarea, placeholder->0x... */}
-      <form className="w-1/2   space-y-2 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-        <label
-          htmlFor="textarea"
-          className="block text-rare/75 font-medium text-sm"
-        >
-          Addresses to Pay
-        </label>
-        <textarea
-          id="textarea"
-          className="block w-3/4 mx-auto outline-none  focus:border focus:border-rare/75"
-          rows={6}
-          placeholder="0x...,0x...,0x...  etc"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        {/* <button onClick={getAddress}>Submit</button> */}
-      </form>
-    </>
-  )
+	const handlePayment = async (event: MouseEvent) => {
+		event.preventDefault()
+		const addressesToPay = addressesString.split('\n')
+		const regex = /^0x[a-fA-F0-9]{40}$/
+		const areAddressesValid = addressesToPay.every((value) => regex.test(value))
+		if (areAddressesValid) {
+			console.log(utils.parseEther(amountToSend))
+			if (contract) {
+				try {
+					const data = await contract.multiSend(
+						addressesToPay,
+						utils.parseEther(amountToSend)
+					)
+					console.log(data)
+				} catch (error: any) {
+					alert(error.error.message)
+				}
+			} else alert('Could not get contract!')
+		}
+	}
+
+	return (
+		<div>
+			<form className='lg:w-1/4 w-1/2 space-y-5 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
+				<p className='mb-16 text-rare/75 text-center text-lg font-medium'>
+					MultiSend Balance:{' '}
+					<span className='text-secondary/80'>{balance}</span> ETH
+				</p>
+
+				<div className='space-y-3'>
+					<label
+						htmlFor='amount'
+						className='block text-rare/60 ml-1 font-medium'>
+						Amount To Pay
+					</label>
+
+					<input
+						type='number'
+						id='amount'
+						min={1}
+						value={amountToSend}
+						onChange={(event) => setAmountToSend(event.target.value)}
+						placeholder='Enter Numeric Value in ETH currency (1 means 1 ETH)'
+						className='block transition w-full outline-none rounded-lg py-2 px-4 text-rare/60 font-medium ring-2 ring-rare/10 focus:ring-rare/30'
+					/>
+				</div>
+
+				<div className='space-y-3'>
+					<label
+						htmlFor='textarea'
+						className='block text-rare/60 ml-1 font-medium'>
+						Addresses to Pay
+					</label>
+
+					<textarea
+						id='textarea'
+						className='block transition resize-none w-full outline-none rounded-lg py-2 px-4 text-rare/60 font-medium ring-2 ring-rare/10 focus:ring-rare/30'
+						rows={6}
+						value={addressesString}
+						placeholder='0x...,etc (Each Address on New Line)'
+						onChange={(event) => setAddressesString(event.target.value)}
+					/>
+				</div>
+
+				<button
+					disabled={!addressesString || !amountToSend}
+					onClick={(event) => handlePayment(event)}
+					className='w-full active:scale-95 font-medium hover:bg-rare transition py-2 px-4 text-white disabled:bg-rare/30 bg-secondary/100 rounded-lg'>
+					Pay
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default ContractInterface
--- utils/contractConfig/index.ts
+++ utils/contractConfig/index.ts
@@ -1,80 +1,80 @@
-export const CONTRACT_ADDRESS = "0x94cCDa2b28519d882EDDeC1Ec98490768f9aBDd7"
+export const CONTRACT_ADDRESS = '0x8F57f0A049f183A419d788C02c332bA4AcDAC4b5'
 export const CONTRACT_ABI = [
-  {
-    inputs: [
-      {
-        internalType: "address payable",
-        name: "_newOwner",
-        type: "address",
-      },
-    ],
-    name: "changeOwner",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address[]",
-        name: "_receivers",
-        type: "address[]",
-      },
-      {
-        internalType: "uint256",
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "multiSend",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    stateMutability: "payable",
-    type: "receive",
-  },
-  {
-    inputs: [],
-    name: "withdraw",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    stateMutability: "payable",
-    type: "fallback",
-  },
-  {
-    inputs: [],
-    stateMutability: "payable",
-    type: "constructor",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address payable",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalSupply",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+	{
+		inputs: [
+			{
+				internalType: 'address payable',
+				name: '_newOwner',
+				type: 'address',
+			},
+		],
+		name: 'changeOwner',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address[]',
+				name: '_receivers',
+				type: 'address[]',
+			},
+			{
+				internalType: 'uint256',
+				name: '_value',
+				type: 'uint256',
+			},
+		],
+		name: 'multiSend',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'receive',
+	},
+	{
+		inputs: [],
+		name: 'withdraw',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		stateMutability: 'payable',
+		type: 'fallback',
+	},
+	{
+		inputs: [],
+		stateMutability: 'payable',
+		type: 'constructor',
+	},
+	{
+		inputs: [],
+		name: 'owner',
+		outputs: [
+			{
+				internalType: 'address payable',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'totalSupply',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
 ]
