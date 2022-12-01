f0082bdb64614b0826a958e724fa446e4a1758c5
Kunal Hole <kunalhole538@gmail.com>
2022-12-01 10:21:24 +0530

    Contract address changed

--- utils/contractConfig/index.ts
+++ utils/contractConfig/index.ts
@@ -1,80 +1,80 @@
-export const CONTRACT_ADDRESS = '0x8F57f0A049f183A419d788C02c332bA4AcDAC4b5'
+export const CONTRACT_ADDRESS = "0xB70467fC56329B0BBF38C5d9340597D01A91C0c7"
 export const CONTRACT_ABI = [
-	{
-		inputs: [
-			{
-				internalType: 'address payable',
-				name: '_newOwner',
-				type: 'address',
-			},
-		],
-		name: 'changeOwner',
-		outputs: [],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		inputs: [
-			{
-				internalType: 'address[]',
-				name: '_receivers',
-				type: 'address[]',
-			},
-			{
-				internalType: 'uint256',
-				name: '_value',
-				type: 'uint256',
-			},
-		],
-		name: 'multiSend',
-		outputs: [],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		stateMutability: 'payable',
-		type: 'receive',
-	},
-	{
-		inputs: [],
-		name: 'withdraw',
-		outputs: [],
-		stateMutability: 'nonpayable',
-		type: 'function',
-	},
-	{
-		stateMutability: 'payable',
-		type: 'fallback',
-	},
-	{
-		inputs: [],
-		stateMutability: 'payable',
-		type: 'constructor',
-	},
-	{
-		inputs: [],
-		name: 'owner',
-		outputs: [
-			{
-				internalType: 'address payable',
-				name: '',
-				type: 'address',
-			},
-		],
-		stateMutability: 'view',
-		type: 'function',
-	},
-	{
-		inputs: [],
-		name: 'totalSupply',
-		outputs: [
-			{
-				internalType: 'uint256',
-				name: '',
-				type: 'uint256',
-			},
-		],
-		stateMutability: 'view',
-		type: 'function',
-	},
+  {
+    inputs: [
+      {
+        internalType: "address payable",
+        name: "_newOwner",
+        type: "address",
+      },
+    ],
+    name: "changeOwner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "_receivers",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+    ],
+    name: "multiSend",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [],
+    stateMutability: "payable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address payable",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ]
